79311c2e4f1e3895ca9e2956422d3a8993c30097
Nuvallo <nuvallo.inc@gmail.com>
2019-10-17 15:15:53 -0400

    sign out module done for all pages

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Sign in function
 function openFormSignIn() {
   document.getElementById("signInForm").style.display = "block";
 }
@@ -6,10 +7,20 @@ function closeFormSignIn() {
   document.getElementById("signInForm").style.display = "none";
 }
 
+// Sign up funtion
 function openFormSignUp() {
   document.getElementById("SignUpForm").style.display = "block";
 }
 
 function closeFormSignUp() {
   document.getElementById("SignUpForm").style.display = "none";
+}
+
+// Sign out function
+function openSignOut() {
+  document.getElementById("SignOut").style.display = "block";
+}
+
+function closeSignOut() {
+  document.getElementById("SignOut").style.display = "none";
 }
