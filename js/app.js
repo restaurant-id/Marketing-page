7879e4e6eb830f0e537fc3f04d66f7696282cac4
Nuvallo <nuvallo.inc@gmail.com>
2019-10-17 14:30:18 -0400

    login and signup modules done

---
+++ js/app.js
@@ -0,0 +1,15 @@
+function openFormSignIn() {
+  document.getElementById("signInForm").style.display = "block";
+}
+
+function closeFormSignIn() {
+  document.getElementById("signInForm").style.display = "none";
+}
+
+function openFormSignUp() {
+  document.getElementById("SignUpForm").style.display = "block";
+}
+
+function closeFormSignUp() {
+  document.getElementById("SignUpForm").style.display = "none";
+}
